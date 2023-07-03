79c4ad8f01752137f87b0fe174d5f4052bb32099
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-03 21:08:11 +0200

    Added more sample code to script.js file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Skrypt działa!");
+
+let output = "To będzie \"tekst\" w cudzysłowie";
+
+let input = prompt("Podaj swoje imię");
+
+switch(input)
+{
+    case "Jan":
+        {
+            console.log("Witaj Janie!");
+        }
+        break;
+    case "Anna":
+        {
+            console.log("Witaj Anno!");
+        }
+        break;
+    default:
+        {
+            console.log("Witaj nieznajomy!");            
+        }
+}
+
+function add(a, b)
+{
+    return a + b;
+}
