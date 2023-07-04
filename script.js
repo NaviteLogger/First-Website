47309f7a13c5ae83448f81c1ace967b780584d91
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-04 17:57:47 +0200

    Added first event listener

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-
 const parentElement  = document.querySelector('body');
 const divElement = document.createElement('div');
 divElement.className = 'my-div';
-divElement.innerHTML = '<p>Hello, world!</p>';
+divElement.textContent = 'Hello, world!';
 divElement.style.backgroundColor = 'red';
 divElement.style.width = '200px';
 parentElement.appendChild(divElement);
 
-});
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//     alert('Hello, world!');
+// });
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e.target);
+//   });
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
